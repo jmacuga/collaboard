0a4477bc05755f5a53a9c83e809ba2f4d3d0b066
Julia Macuga <julia.macuga@gmail.com>
2024-09-16 20:49:46 +0200

    remove comment

--- collaboard/app/socket.ts
+++ collaboard/app/socket.ts
@@ -1,12 +1,11 @@
 "use client";
 
-import { ScissorsIcon } from "@heroicons/react/24/outline";
 import { io, Socket } from "socket.io-client";
 
 var socket: Socket;
 
 if (typeof window !== "undefined") {
-  socket = io(); // Replace with your server URL
+  socket = io();
 }
 
 export { socket };

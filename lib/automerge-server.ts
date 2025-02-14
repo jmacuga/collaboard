b718b9d7e95dd35d68eb419b13730c27890b3905
Julia Macuga <julia.macuga@gmail.com>
2025-02-14 18:46:45 +0100

    slihtly adapted ustom server

--- lib/automerge-server.ts
+++ lib/automerge-server.ts
@@ -4,11 +4,14 @@ import { NodeWSServerAdapter } from "@automerge/automerge-repo-network-websocket
 import { MongoDBStorageAdapter } from "@/lib/automerge-repo-storage-mongodb";
 
 export function createAutomergeServer(wss: WebSocketServer, hostname: string) {
-  const mongoAdapter = new MongoDBStorageAdapter(process.env.MONGO_URL || "", {
-    dbName: "collaboard",
-    collectionName: "docs",
-    keyStorageStrategy: "array",
-  });
+  const mongoAdapter = new MongoDBStorageAdapter(
+    process.env.MONGODB_URI || "",
+    {
+      dbName: "collaboard",
+      collectionName: "docs",
+      keyStorageStrategy: "array",
+    }
+  );
 
   const config = {
     network: [new NodeWSServerAdapter(wss as any)],

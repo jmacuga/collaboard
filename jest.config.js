fd303dfd9b5e2f18eb5b3efdd526140af98e9020
Julia Macuga <julia.macuga@gmail.com>
2025-04-09 20:43:02 +0200

    basic jest tests setup

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!lucide-react|@radix-ui|@babel|@testing-library)/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ jest.setup.js
@@ -0,0 +1,23 @@
+// Learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: {},
+      asPath: "",
+      push: jest.fn(),
+      replace: jest.fn(),
+    };
+  },
+}));
+
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />;
+  },
+}));

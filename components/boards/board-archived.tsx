1ff0136e3f9c74ca36b924c01133fc20e1bcaf0a
Julia Macuga <julia.macuga@gmail.com>
2025-04-15 17:10:13 +0200

    added teams button in team archived and board archived viewes

--- components/boards/board-archived.tsx
+++ components/boards/board-archived.tsx
@@ -1,4 +1,6 @@
 import { AppLayout } from "@/components/layouts/app-layout";
+import { Button } from "../ui/button";
+import { Link } from "lucide-react";
 
 export default function BoardArchived() {
   return (
@@ -6,6 +8,9 @@ export default function BoardArchived() {
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-2xl font-bold">Board is deleted</h1>
       </div>
+      <Button className="mt-4">
+        <Link href="/teams">Go to teams</Link>
+      </Button>
     </AppLayout>
   );
 }
--- components/teams/team-archived.tsx
+++ components/teams/team-archived.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 export default function TeamArchived() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Team is archived</h1>
+      <h1 className="text-2xl font-bold">Team is deleted</h1>
       <p className="text-sm text-gray-500">
-        This team is archived and cannot be accessed.
+        This team is deleted and cannot be accessed.
       </p>
+      <Button className="mt-4">
+        <Link href="/teams">Go to teams</Link>
+      </Button>
     </div>
   );
 }
